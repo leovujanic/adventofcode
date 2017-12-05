6e3f801cd0a2fa7c6e5ae46ae183746a92d6abf7
leovujanic <leonard.vujanic@gmail.com>
2017-12-05 10:27:09 +0100

    Added fifth day skeleton

---
+++ src/days/FifthDay.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * User: leonardvujanic
+ * DateTime: 05/12/2017 10:14
+ *
+ *
+ */
+
+namespace leovujanic\adventofcode;
+
+/**
+ * Class FifthDay
+ *
+ * @package leovujanic\adventofcode
+ */
+class FifthDay
+{
+    
+    
+    public function first()
+    {
+    }
+    
+    
+    public function second()
+    {
+    }
+}
---
+++ tests/unit/FifthDayTest.php
@@ -0,0 +1,33 @@
+<?php
+
+use leovujanic\adventofcode\FifthDay;
+
+/**
+ * Class FifthDayTest
+ */
+class FifthDayTest extends \Codeception\Test\Unit
+{
+    /**
+     * @var \UnitTester
+     */
+    protected $tester;
+    
+    /**
+     * @var FifthDayTest
+     */
+    protected $day;
+    
+    protected function _before()
+    {
+        $this->day = new FifthDay();
+    }
+    
+    
+    public function atestFirstSolution()
+    {
+    }
+    
+    public function atestSecondSolution()
+    {
+    }
+}
